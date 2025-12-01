52644a913031c80dc226716f6a3caede48cc1b0a
rchmn17 <ptr330rachman@gmail.com>
2025-12-01 23:24:25 +0700

    updateFlow

--- script/DAscript.js
+++ script/DAscript.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const backButton = document.querySelector('.group-16 .text-wrapper');
   if (backButton && backButton.textContent === 'Back') {
     backButton.addEventListener('click', function() {
-      window.location.href = 'index.html';
+      window.location.href = 'RiwayatPendampingan_madif.html';
     });
   }
 
